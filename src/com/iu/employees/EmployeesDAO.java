6b36279e65f34069d744e3863a42f06e3dc8601c
kdy991101 <kdy991101@naver.com>
2022-08-02 17:10:15 +0900

    ds

--- src/com/iu/employees/EmployeesDAO.java
+++ src/com/iu/employees/EmployeesDAO.java
@@ -1,13 +1,54 @@
 package com.iu.employees;
 
+import java.net.http.WebSocketHandshakeException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
+import com.iu.departments.DepartmentsDTO;
 import com.iu.util.DBConnector;
 
 public class EmployeesDAO {
+	
+	public void grtJoinTest(EmployeesDTO employeesDTO) throws Exception{
+//		1.db연결
+		Connection con = DBConnector.getConnection();
+		
+//		2.sql문 생성
+		String sql = "SELECT E.LAST_NAME,SALARY, D.DEPARTMENT_NAME "
+				+ "FROM EMPLOYEES E "
+				+ "     INNER JOIN "
+				+ "     DEPARTMENTS D "
+				+ "     ON E.DEPARTMENT_ID = D.DEPARTMENT_ID "
+				+ "     WHERE E.EMPLOYEE_ID=?";
+		
+//		3.미리 전송 단계
+		PreparedStatement st = con.prepareStatement(sql);
+		
+//		4.?세팅
+		st.setInt(1, employeesDTO.getEmplotee_id());
+		
+//		5.전송 후 결과처리
+		ResultSet rs = st.executeQuery();
+		
+		if(rs.next()) {
+			employeesDTO = new EmployeesDTO();
+			employeesDTO.setLast_name(rs.getString("last_name"));
+			employeesDTO.setSalary(rs.getInt("salary"));
+			DepartmentsDTO dt = new DepartmentsDTO();
+			dt.setDepartment_name(rs.getString("department_name"));
+		}
+		
+		
+//		6.연결 해제
+		DBConnector.disConnect(rs, st, con);
+		
+	}
+	
+	
+	
+	
 	
 	public void getSalaryInfo() throws Exception{//7-28
 		
